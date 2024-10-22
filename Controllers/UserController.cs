0d4a86bba99f79083f06620ca649121a3f6a6da6
Pollawat <Pollawatstp@gmail.com>
2024-10-22 14:34:37 +0700

    add user page

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace elephathai3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult UserItemList()
+        {
+            return View();
+        }
+
+        public ActionResult UserDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult UserItemCreate()
+        {
+            return View();
+        }
+
+        public ActionResult UserItemDetail()
+        {
+            return View();
+        }
+
+        public ActionResult UserItemPreview()
+        {
+            return View();
+        }
+    }
+}
